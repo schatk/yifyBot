47427463af52293aef5871bbd90b03aa627d907a
Karl Schatke <250854E@curtin.edu.au>
2019-07-15 15:50:22 +0800

    Mucking around

--- yify.py
+++ yify.py
@@ -2,11 +2,15 @@ import requests
 import json
 
 print("Which movie do you want to see?")
-
 movie = input()
+
 payload = {'movie_id': movie}
-
 r = requests.get('https://yts.lt/api/v2/movie_details.json', params=payload)
-response = json.loads(r.text)
 
-print(response)
+r = json.loads(r.text)
+r = json.dumps(r, indent=2, sort_keys=True)
+
+with open("test.json","w") as outfile:
+    json.dump(r, outfile)
+
+# print(r)
